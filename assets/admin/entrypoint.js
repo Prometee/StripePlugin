4c9eb0b38da967b2fa631ab88e8580819d85c31e
Piotr Tułacz <piotr.tulacz@sylius.com>
2026-06-02 14:25:03 +0200

    init

--- assets/admin/entrypoint.js
+++ assets/admin/entrypoint.js
@@ -1,6 +1,7 @@
 import { Application } from '@hotwired/stimulus';
 import ColorPickerController from './controllers/stripe-appearance-color-picker-controller.js';
 import './form.scss';
+import './stripe.scss';
 
 const app = Application.start();
 app.register('stripe-appearance-color-picker', ColorPickerController);
