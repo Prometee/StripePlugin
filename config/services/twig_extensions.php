b52359778eb46e7db772f65d0086bc8ca0eba3f0
Piotr Tułacz <piotr.tulacz@sylius.com>
2026-06-03 14:14:07 +0200

    rebase twig extensions

--- config/services/twig_extensions.php
+++ config/services/twig_extensions.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 use FluxSE\SyliusStripePlugin\Stripe\SecretKey\LegacyKeyDetectorInterface;
 use FluxSE\SyliusStripePlugin\Twig\Extension\LegacyStripeKeyExtension;
+use Sylius\Bundle\PaymentBundle\Provider\GatewayFactoryNameProviderInterface;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
+use function Symfony\Component\DependencyInjection\Loader\Configurator\param;
 use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
 return static function (ContainerConfigurator $container): void {
@@ -14,6 +16,9 @@ return static function (ContainerConfigurator $container): void {
     $services->set('flux_se.sylius_stripe.twig.extension.legacy_stripe_key', LegacyStripeKeyExtension::class)
         ->args([
             service(LegacyKeyDetectorInterface::class),
+            service('sylius.repository.payment_method'),
+            service(GatewayFactoryNameProviderInterface::class),
+            param('flux_se.sylius_stripe.factories'),
         ])
         ->tag('twig.extension');
 };
